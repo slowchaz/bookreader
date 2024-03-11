3ed8f98d1545946434cc2bda2552df0c30e8ab77
chaz <corfitzgerald@gmail.com>
2024-03-11 14:16:40 -0500

    counting words

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    print(num_words)
 
+def count_words(text):
+    return len(text.split())  
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
